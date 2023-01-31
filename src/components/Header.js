f831ccdb7523f21be2d762dcb0d9d4ab1b8a9630
Moustapha Diakhaby <diakhabymous@gmail.com>
2023-01-31 09:30:40 +0100

    ajout du logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import Title from "./Title";
 import Baseline from "./Baseline";
+import logo from "../assets/img/sfvlogo.png";
 
 const Header = () => {
   return (
     <div className="header container">
-      <Title name="Street Fighter" />
-      <Baseline text="An awesome game" />
+      <div className="logo">
+        <img src={logo} alt="" />
+      </div>
+      <div className="headertext">
+        <Title name="Street Fighter" />
+        <Baseline text="An awesome game" />
+      </div>
     </div>
   );
 };
