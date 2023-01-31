b3c1c93f303c1e9d5fda2b3db7faa975829329a8
Moustapha Diakhaby <diakhabymous@gmail.com>
2023-01-31 11:00:00 +0100

    ajout de bison

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,21 +1,21 @@
 import Item from "./Item";
-import banner from "../assets/img/sfvchampion.jpg";
-import banner2 from "../assets/img/nash.webp";
+import bison from "../assets/img/mbison.jpg";
+import nash from "../assets/img/nash.webp";
 
 const MainContent = () => {
   return (
     <div className="maincontent">
       <Item
-        name="Street fighter v"
-        text="Jeu phare de la licence"
-        img={banner}
-        desc="Le 16 février 2016 sort Street Fighter V sur Playstation 4 et sur PC. Le jeu ne sortira pas sur la console concurrente de celle-ci, la Xbox One de Microsoft, en raison d'un contrat de développement entre Capcom et Sony, les constructeurs de la PlayStation."
+        name="Mighty bison"
+        text="Grand dictateur et chef de Shadaloo"
+        img={bison}
+        desc="M. Bison, parfois surnommé Mighty Bison, est le principal antagoniste de la série de jeux vidéo de combat Street Fighter, débutant dans Street Fighter II : La guerre mondiale. Il est le chef tyrannique, diabolique, manipulateur et sans cœur de l'organisation Shadaloo, et le méchant le plus autoritaire et le plus puissant de la série. Il apparaît également comme l'antagoniste principal des adaptations télévisées et des films. Il est également l'ennemi le plus détesté de Ryu et Chun-Li et de nombreux autres personnages de la série."
       />
 
       <Item
-        name="nash"
-        text="Charlie Nash"
-        img={banner2}
+        name="charlie nash"
+        text="Le meilleur ami de Guile"
+        img={nash}
         desc="Le Lieutenant Charlie Nash de l'US Air Force est un compagnon d'arme de Guile. Il enquête sur la corruption au sein de l'armée américaine, certains officiers vendraient des armes à l'organisation criminelle Shadowlaw, dirigée par M. Bison. Epris de justice, il trouvera de l'aide dans sa traque auprès de Chun-Li, elle aussi sur les traces de Bison.
 
         La fin officielle statuée par Capcom indique que Charlie est mort en permettant à Guile de s'échapper de la base de Bison quand elle explose, à la fin de Street Fighter Alpha 3. Ce qui explique pourquoi Guile est revenu dans Street Fighter 2, pour venger son ami car il a appris que Bison était toujours vivant."
